6a797b7c16c34a0e775f67c78b2c536f390c4744
BrunoG <brunoguillardopv@gmail.com>
2026-09-25 16:40:03 +0200

    Rename story-specific icon to special

--- books/Livre01-La-Grotte-de-Valombre/manifest.js
+++ books/Livre01-La-Grotte-de-Valombre/manifest.js
@@ -13,14 +13,14 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  assetVersion:182,
+  assetVersion:183,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
     statsTexture:'assets/textures/texture-caracteristiques.jpg',
     parchmentTexture:'assets/textures/texture-parchemin.jpg',
     icons:{
       vie:'assets/icons/vie.png', dexterite:'assets/icons/dexterite.png', force:'assets/icons/force.png',
-      arme:'assets/icons/arme.png', protection:'assets/icons/protection.png', terreNoire:'assets/icons/terre-noire.png'
+      arme:'assets/icons/arme.png', protection:'assets/icons/protection.png', special:'assets/icons/special.png'
     }
   }
 });
