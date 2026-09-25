d9e4eb28a4f4f33e96caabe62d05a2b258261325
BrunoG <brunoguillardopv@gmail.com>
2026-09-25 16:02:43 +0200

    Bump Providence dialogue version

--- books/Livre02-Le-Secret-du-Providence/manifest.js
+++ books/Livre02-Le-Secret-du-Providence/manifest.js
@@ -14,7 +14,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  contentVersion:6,
+  contentVersion:7,
   assetVersion:1,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
