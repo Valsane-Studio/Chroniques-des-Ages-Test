b6f3535bb991439099eb7c2ea9b94eeadd59b0f2
BrunoG <brunoguillardopv@gmail.com>
2026-09-25 11:52:43 +0200

    Publie books/Livre01-La-Grotte-de-Valombre/manifest.js

---
+++ books/Livre01-La-Grotte-de-Valombre/manifest.js
@@ -0,0 +1,31 @@
+BookManifestRegistry.register({
+  id:'livre01',
+  runtimeId:'ecuyer-01',
+  number:1,
+  label:'Livre 01',
+  kicker:'Chroniques des Âges · Livre 01',
+  title:'La Grotte de Valombre',
+  pitch:'Sir Aldren a disparu. Les réponses se trouvent sous Valombre.',
+  status:'available',
+  actionLabel:'Jouer',
+  cover:'assets/presentation.webp',
+  coverCandidates:['assets/presentation.webp'],
+  bookScript:'book.js',
+  journalScript:'journal.js',
+  extraScripts:['map.js'],
+  contentVersion:1,
+  assetVersion:1,
+  theme:{
+    buttonTexture:'assets/textures/texture-bouton.jpg',
+    statsTexture:'assets/textures/texture-caracteristiques.jpg',
+    parchmentTexture:'assets/textures/texture-parchemin.jpg',
+    icons:{
+      vie:'assets/icons/vie.png',
+      dexterite:'assets/icons/dexterite.png',
+      force:'assets/icons/force.png',
+      arme:'assets/icons/arme.png',
+      protection:'assets/icons/protection.png',
+      terreNoire:'assets/icons/terre-noire.png'
+    }
+  }
+});
