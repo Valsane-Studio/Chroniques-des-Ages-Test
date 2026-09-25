9f102dd83fc632d27cd9d08a968a8ced8a55db48
BrunoG <brunoguillardopv@gmail.com>
2026-09-25 15:58:18 +0200

    Bump Providence tavern dialogue version

--- books/Livre02-Le-Secret-du-Providence/manifest.js
+++ books/Livre02-Le-Secret-du-Providence/manifest.js
@@ -14,7 +14,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  contentVersion:4,
+  contentVersion:5,
   assetVersion:1,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
