1135596aefe885800a1d1cb92685e35e717e33c8
BrunoG <brunoguillardopv@gmail.com>
2026-09-25 17:23:38 +0200

    Refresh absolute texture assets

--- books/Livre01-La-Grotte-de-Valombre/manifest.js
+++ books/Livre01-La-Grotte-de-Valombre/manifest.js
@@ -13,7 +13,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  assetVersion:184,
+  assetVersion:185,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
     statsTexture:'assets/textures/texture-caracteristiques.jpg',
