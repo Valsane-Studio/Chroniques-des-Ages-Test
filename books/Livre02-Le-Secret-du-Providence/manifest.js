2feeaa07d0ffa9a4abbbeb7f1686ddd831dc7516
BrunoG <brunoguillardopv@gmail.com>
2026-09-25 16:04:45 +0200

    Bump Providence title cleanup version

--- books/Livre02-Le-Secret-du-Providence/manifest.js
+++ books/Livre02-Le-Secret-du-Providence/manifest.js
@@ -14,7 +14,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  contentVersion:8,
+  contentVersion:9,
   assetVersion:1,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
