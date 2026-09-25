441a5ffff9772c755ef1b7875623f0f23801ef78
BrunoG <brunoguillardopv@gmail.com>
2026-09-25 16:22:05 +0200

    Bump Providence immersive rewrite version

--- books/Livre02-Le-Secret-du-Providence/manifest.js
+++ books/Livre02-Le-Secret-du-Providence/manifest.js
@@ -14,7 +14,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  contentVersion:9,
+  contentVersion:10,
   assetVersion:1,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
