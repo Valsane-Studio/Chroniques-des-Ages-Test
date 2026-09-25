94dd1e4476fae51dd164e28bee7529402759421c
BrunoG <brunoguillardopv@gmail.com>
2026-09-25 14:27:07 +0200

    Passe le thème du Livre 01 en version 181

--- books/Livre01-La-Grotte-de-Valombre/manifest.js
+++ books/Livre01-La-Grotte-de-Valombre/manifest.js
@@ -13,7 +13,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  assetVersion:180,
+  assetVersion:181,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
     statsTexture:'assets/textures/texture-caracteristiques.jpg',
