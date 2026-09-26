2145672beaefaa26f5259400f9914a26abade144
BrunoG <brunoguillardopv@gmail.com>
2026-09-26 10:08:13 +0200

    Version one-roll pirate melee

--- books/Livre02-Le-Secret-du-Providence/manifest.js
+++ books/Livre02-Le-Secret-du-Providence/manifest.js
@@ -21,7 +21,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  contentVersion:20,
+  contentVersion:21,
   assetVersion:7,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
