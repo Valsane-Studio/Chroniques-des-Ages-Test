121ed08c3c03ff4ae667ed1adf01863df79c7e1c
BrunoG <brunoguillardopv@gmail.com>
2026-09-26 08:14:09 +0200

    Version direct combat initialization fix

--- books/Livre02-Le-Secret-du-Providence/manifest.js
+++ books/Livre02-Le-Secret-du-Providence/manifest.js
@@ -21,7 +21,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  contentVersion:16,
+  contentVersion:17,
   assetVersion:7,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
