d67707fbc53ad1693ae372a9aeb0bccb9bbbebc5
BrunoG <brunoguillardopv@gmail.com>
2026-09-26 07:03:24 +0200

    Version crew combat compatibility fix

--- books/Livre02-Le-Secret-du-Providence/manifest.js
+++ books/Livre02-Le-Secret-du-Providence/manifest.js
@@ -21,7 +21,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  contentVersion:12,
+  contentVersion:13,
   assetVersion:7,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
