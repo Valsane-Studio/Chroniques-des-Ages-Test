d67fd960c428aba1c3080a0dd0daaa6ced887283
BrunoG <brunoguillardopv@gmail.com>
2026-09-26 07:08:45 +0200

    Version Providence work navigation titles

--- books/Livre02-Le-Secret-du-Providence/manifest.js
+++ books/Livre02-Le-Secret-du-Providence/manifest.js
@@ -21,7 +21,7 @@ BookManifestRegistry.register({
   journalScript:'journal.js',
   extraScripts:['map.js'],
   themeStylesheet:'assets/theme.css',
-  contentVersion:14,
+  contentVersion:15,
   assetVersion:7,
   theme:{
     buttonTexture:'assets/textures/texture-bouton.jpg',
